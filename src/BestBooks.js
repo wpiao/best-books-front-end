eb17fab96ada30ae17247f68911ed04660e88e9d
Wenhao Piao <wenhaopiao520@gmail.com>
2021-04-29 21:11:26 -0700

    Add Carousel

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import superagent from 'superagent';
+import { Container, Carousel } from 'react-bootstrap';
 
 class BestBooks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    }
+  }
 
   componentDidMount() {
     const url = `${process.env.REACT_APP_BACKEND_URL}/books`
-    console.log(url);
     superagent.get(url)
       .query({ email: this.props.userInfo.email })
-      .then(res => console.log(res));
+      .then(res => {
+        this.setState({ books: res.body });
+      })
+      .catch(err => console.error(err))
   }
 
   render() {
     return (
-      <>
-        <h1>Hello world</h1>
-      </>
+      <Container>
+        <Carousel>
+          {this.state.books.map(book =>
+            <Carousel.Item key={book._id}>
+              <img
+                className="d-block w-100"
+                src={book.photo}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>{book.name}</h3>
+                <p>{book.description}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          )}
+        </Carousel>
+      </Container>
     );
   }
 }
